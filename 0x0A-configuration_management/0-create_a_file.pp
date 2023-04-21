c9732f5bde5bb7b3092e35d19ca482f751e2d178
Michael Aloh <mykelaloh@gmail.com>
2023-04-21 16:18:44 -0700

    align arriws

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a fike with below title and attr
 file {'/tmp/school':
-  ensure => 'file',
-  mode => '0744',
-  owner => 'www-data',
-  group =>'www-data',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
   }

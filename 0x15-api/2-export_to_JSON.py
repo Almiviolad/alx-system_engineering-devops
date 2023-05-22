4963664546e7417ce6630eb3e44fa9f8b97b7469
Aloh Michael <mykelaloh@gmail.com>
2023-05-22 10:13:33 -0700

    updated

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,24 +5,25 @@ import json
 import requests
 import sys
 
-id = sys.argv[1]
-todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
-user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+if __name__ == "__main__":
+    id = sys.argv[1]
+    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
 
-todo = requests.get(todo_url)
-todo_json = todo.json()
-user_info = requests.get(user_url)
-user_json = user_info.json()
+    todo = requests.get(todo_url)
+    todo_json = todo.json()
+    user_info = requests.get(user_url)
+    user_json = user_info.json()
 
-username = user_json.get('name')
-filename = 'USER_ID.json'
-json_list = []
-for task in todo_json:
-    status = task['completed']
-    title = task["title"]
-    json_list.append({
-        "task": "{}".format(task['title']),
-        "completed": "{}".format(task['completed']),
-        "username": "{}".format(username)})
-with open(filename, 'w') as jsonfile:
-    json.dump({id: json_list}, jsonfile)
+    username = user_json.get('name')
+    filename = 'USER_ID.json'
+    json_list = []
+    for task in todo_json:
+        status = task['completed']
+        title = task["title"]
+        json_list.append({
+            "task": "{}".format(task['title']),
+            "completed": "{}".format(task['completed']),
+            "username": "{}".format(username)})
+        with open(filename, 'w') as jsonfile:
+            json.dump({id: json_list}, jsonfile)
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,31 +5,32 @@ import json
 import requests
 import sys
 
-user_url = 'https://jsonplaceholder.typicode.com/users/'
-user_info = requests.get(user_url)
-user_json = user_info.json()
-dictionary = {}
-
-for user in user_json:
-    id = user['id']
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        id)
-    todo = requests.get(todo_url)
-    todo_json = todo.json()
+if __name__ == "__main__":
+    user_url = 'https://jsonplaceholder.typicode.com/users/'
     user_info = requests.get(user_url)
     user_json = user_info.json()
-    username = user_json.get('name')
-    filename = 'USER_ID.json'
-    json_list = []
-    for task in todo_json:
-        status = task['completed']
-        title = task["title"]
-        json_list.append({
-            "task": "{}".format(task['title']),
-            "completed": "{}".format(task['completed']),
-            "username": "{}".format(username)})
-    dictionary[str(id)] = json_list
+    dictionary = {}
 
-with open(filename, 'w') as jsonfile:
-    json.dump(dictionary, jsonfile)
+    for user in user_json:
+        id = user['id']
+        user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+        todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+        todo = requests.get(todo_url)
+        todo_json = todo.json()
+        user_info = requests.get(user_url)
+        user_json = user_info.json()
+        username = user_json.get('name')
+        filename = 'USER_ID.json'
+        json_list = []
+        for task in todo_json:
+            status = task['completed']
+            title = task["title"]
+            json_list.append({
+                "task": "{}".format(task['title']),
+                "completed": "{}".format(task['completed']),
+                "username": "{}".format(username)})
+            dictionary[str(id)] = json_list
+
+            with open(filename, 'w') as jsonfile:
+                json.dump(dictionary, jsonfile)
+    

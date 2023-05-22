01669386431c8b73d7b82d0dd1770cbec162fde6
Aloh Michael <mykelaloh@gmail.com>
2023-05-22 02:23:36 -0700

    task0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that fetches data from api"""
+import sys
+import requests
+
+id = sys.argv[1]
+todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+
+todo = requests.get(todo_url)
+todo_json = todo.json()
+user_info = requests.get(user_url)
+user_json = user_info.json()
+
+completed_tasks = [task for task in todo_json if task['completed']]
+complete = len(completed_tasks)
+total_tasks = len(todo_json)
+username = user_json.get('name')
+
+
+print(
+    f"Employee {username} is done with tasks({complete}/{total_tasks}):")
+for task in completed_tasks:
+    print(f"\t {task['title']}")

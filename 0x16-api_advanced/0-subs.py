f926ea92e08f57541f4357ca2352ce07e4b7c609
Aloh Michael <mykelaloh@gmail.com>
2023-06-06 01:18:35 -0700

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that takes in a subreddit and retyrns no of users"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'android:alx_func:v1.0 (by /u/almiviolad)'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        r_json = response.json()
+        return (r_json['data']['subscribers'])
+    else:
+        return 0

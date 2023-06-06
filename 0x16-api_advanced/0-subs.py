84d660f632b29af0cd79cf00a3d4b800e2cb9478
Aloh Michael <mykelaloh@gmail.com>
2023-06-06 06:43:35 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Function that takes in a subreddit and retyrns no of users"""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'android:alx_func:v1.0 (by /u/almiviolad)'}
     response = requests.get(url, headers=headers)

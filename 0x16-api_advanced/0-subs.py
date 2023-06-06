bf4475815958e40eae4dddc3f36619e9f8388578
Aloh Michael <mykelaloh@gmail.com>
2023-06-06 06:46:12 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""a function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit. """
-import requests
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Function that takes in a subreddit and retyrns no of users"""
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'android:alx_func:v1.0 (by /u/almiviolad)'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        r_json = response.json()
-        return (r_json['data']['subscribers'])
-    else:
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0

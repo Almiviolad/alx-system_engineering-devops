0ee83d4b83c183a97fe2970a64462cd91eecb60f
Aloh Michael <mykelaloh@gmail.com>
2023-06-07 05:49:11 -0700

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,15 +12,22 @@ def top_ten(subreddit):
     function that queries the Reddit API and prints the titles of the first
     10 hot posts listed for a given subreddit
     """
-    
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'android:alx_func:v1.0 (by /u/almiviolad)'}
-    response = get(url, headers=headers)
 
-    if response.status_code == 200:
-        r_json = response.json()
-        for post in r_json['data']['children']:
-            print(post['data']['title'])
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
 
-    else:
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
         print("None")

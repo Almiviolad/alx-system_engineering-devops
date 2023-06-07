8eab61dc1c6859c8f5f930d17eaec5b5bbca825b
Aloh Michael <mykelaloh@gmail.com>
2023-06-07 05:50:58 -0700

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,6 @@ def top_ten(subreddit):
     function that queries the Reddit API and prints the titles of the first
     10 hot posts listed for a given subreddit
     """
-
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
 

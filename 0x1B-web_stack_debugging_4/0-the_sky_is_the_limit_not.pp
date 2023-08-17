7ebdcd56468b96f5fd2b701ad316317d009f742d
Aloh Michael <mykelaloh@gmail.com>
2023-08-17 12:03:10 -0700

    done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# manuscript that increase the amount of traffic Nginx server can handle
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/'
+    }
+
+# To restart ngunx and make the settings take effect
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+    path    => '/etc/init.d/'
+    }
